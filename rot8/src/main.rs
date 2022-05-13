86bb6dd57892e963fdb3a8a11b4fbf95c2b8813a
Luiz F. A. de Prá <rawarkanis@gmail.com>
2022-05-13 18:55:47 -0300

    temp commit

--- rot8/src/main.rs
+++ rot8/src/main.rs
@@ -1,3 +1,13 @@
+use rotlib::machine::Machine;
+
+const SCREEN_WIDTH: u32 = 64;
+const SCREEN_HEIGHT: u32 = 32;
+const SCREEN_SCALE: u32 = 1;
+
+struct Interpreter {
+    machine: Machine,
+}
+
 fn main() {
     println!("Hello, world!");
 }
